416e832c19f72469f7f1d854c6334d78eec59d25
Beqa27 <beqauxuuxu@gmail.com>
2025-07-04 15:16:23 +0000

    first

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include "../headers/includes.h"
+
+int main(){
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Event e;
+    bool running = true;
+
+    while (running)
+    {
+        while(SDL_PollEvent(&e)){
+            if(e.type == SDL_QUIT){
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
+    return 0;
+}
