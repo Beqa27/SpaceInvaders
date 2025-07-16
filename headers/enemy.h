8167702df2d89b707c8d4c6385d1ed3fdbf8a123
Beqa27 <beqauxuuxu@gmail.com>
2025-07-16 21:29:37 +0400

    single dumb alien moving

--- headers/enemy.h
+++ headers/enemy.h
@@ -6,6 +6,9 @@ class Enemy{
     int speed;
     SDL_Rect alienRect;
     SDL_Texture* enemyTexture;
+    Uint32 lastMoveTime;
+    const Uint32 moveCooldown;
+    
 
     Enemy(SDL_Renderer * renderer, int speed, SDL_Rect alienRect, const char * enemySpreite);
     ~Enemy();
--- src/enemy.cpp
+++ src/enemy.cpp
@@ -1,6 +1,6 @@
 #include "../headers/enemy.h"
 
-Enemy::Enemy(SDL_Renderer * renderer, int speed, SDL_Rect alienRect, const char * enemyPath) : speed(speed), alienRect(alienRect) {
+Enemy::Enemy(SDL_Renderer * renderer, int speed, SDL_Rect alienRect, const char * enemyPath) : speed(speed), alienRect(alienRect), lastMoveTime(0), moveCooldown(1000) {
     enemyTexture = IMG_LoadTexture(renderer, enemyPath);
     if(!enemyTexture){
         std::cerr << "failed to load texture" << std::endl;
@@ -12,14 +12,28 @@ Enemy::~Enemy(){
         SDL_DestroyTexture(enemyTexture);
     }
 }
-void Enemy::update(){
-    alienRect.x += speed;
+void Enemy::update() {
+    Uint32 currentTime = SDL_GetTicks();
+    if (currentTime - lastMoveTime > moveCooldown) {
+        // Move horizontally in steps
+        alienRect.x += speed;
 
-    // Check if it hit the left or right border
-    if(alienRect.x <= 0 || alienRect.x + alienRect.w >= 800){
-        speed = -speed;
+        // Check if it hits the wall
+        if (alienRect.x <= 0 || alienRect.x + alienRect.w >= 800) {
+            // Reverse direction
+            speed = -speed;
+
+            // Move down when hitting wall
+            alienRect.y += 10;
+
+            // Make sure it doesn't "double move" left/right on next tick
+            alienRect.x += speed; 
+        }
+
+        lastMoveTime = currentTime;
     }
 }
+
 
 void Enemy::draw(SDL_Renderer* renderer) {
     if (enemyTexture) {
